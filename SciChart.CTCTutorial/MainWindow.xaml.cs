096892d01f3396c459baae8a6ff0209ba38f5c61
surbachCTC <83680338+surbachCTC@users.noreply.github.com>
2021-05-05 15:13:29 -0700

    SINE-COS plots

--- SciChart.CTCTutorial/MainWindow.xaml.cs
+++ SciChart.CTCTutorial/MainWindow.xaml.cs
@@ -4,14 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using SciChart.Charting.Model.DataSeries;
 
 namespace SciChart.CTCTutorial
 {
@@ -23,6 +16,22 @@ namespace SciChart.CTCTutorial
         public MainWindow()
         {
             InitializeComponent();
+            this.Loaded += OnLoaded;
+        }
+        private void OnLoaded(object sender, RoutedEventArgs routedEventArgs)
+        {
+            // Create XyDataSeries to host data for our charts
+            var scatterData = new XyDataSeries<double, double>();
+            var lineData = new XyDataSeries<double, double>();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                lineData.Append(i, Math.Sin(i * 0.1));
+                scatterData.Append(i, Math.Cos(i * 0.1));
+            }
+            // Assign dataseries to RenderSeries
+            LineSeries.DataSeries = lineData;
+            ScatterSeries.DataSeries = scatterData;
         }
     }
 }
