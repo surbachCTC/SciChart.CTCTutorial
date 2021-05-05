7e0e946f5261aecebd6e993909f27a9bd1fdde82
surbachCTC <83680338+surbachCTC@users.noreply.github.com>
2021-05-05 14:56:31 -0700

    Add project files.

---
+++ SciChart.CTCTutorial/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using SciChart.Charting.Visuals;
+
+
+namespace SciChart.CTCTutorial
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        // Set this code once in App.xaml.cs or application startup
+        //SciChartSurface.SetRuntimeLicenseKey("ePEhJnfYWfInVhQ6u24gAnav1LMyC2M4FJ8nB26nRwtO4X4CmJDkyEafeoQQmg/gnvqkVC9FnLrjZQ8OGa6jirsN3s8MPPMDmefy4CUElxBJG8VgLXJa3n7wVku+GPZzcTt7YsOSdFOIWRS6+dNUTJUoXmIuhiHcdGYA1Y3W/HsOKogCHpYlqKQXy+a0P/ZYBJcc/2wwhUuQ9dK6H6ihrSEDj0J4Td4eQVzltxj4XBFwbSyofSTFur0U8Zd8xUBb1FuCdLxSsAdIG13ui7/6y9Y4M8sO18/ocuDfda91DKUlUWvfbNVFfPqtl46afIY21EGlBMc8Vuo70BsasuhOC5FkRkJcLVnJxX/aqXryiUFr/wiD7a7w8ov1uHro3i0Vt1BgFSMYJV0Y+EjpAJrphAwOYMtz0zZN37dC+65olnFtpVr+3c9zcEL5Pp2DB2tPtJ2nMMF/M5B5n4C0iy4ImZvWae47i6Z6f9gOL14XfRZzBDtpSTbAhfiSUR7gA8PX345iVmVdJG4z8wajUxOfOJzF+ELYzeDoHCfxEtM6yqpWXCtTfwfaEesbE+bQh/Cl4UQADqszYA==");
+    }
+}
---
+++ SciChart.CTCTutorial/MainViewModel.cs
@@ -0,0 +1,24 @@
+﻿using SciChart.Data.Model;
+namespace SciChart.CTCTutorial
+{
+    // Create the MainViewModel. BindableObject is our own viewmodel base class in scichart. You can use your own!
+    public class MainViewModel : BindableObject
+    {
+        private string _test;
+        public MainViewModel()
+        {
+            TestString = "Hello MVVM World!";
+        }
+        public string TestString
+        {
+            get { return _test; }
+            set
+            {
+                _test = value;
+                OnPropertyChanged("TestString");
+            }
+        }
+    }
+}
+
+
