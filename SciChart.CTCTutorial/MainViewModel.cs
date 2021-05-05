6f1ec9685fb1c296329ea3df8c3c0e4db8446873
surbachCTC <83680338+surbachCTC@users.noreply.github.com>
2021-05-05 15:04:22 -0700

    Basic Chart displayed

--- SciChart.CTCTutorial/MainViewModel.cs
+++ SciChart.CTCTutorial/MainViewModel.cs
@@ -1,21 +1,44 @@
-﻿using SciChart.Data.Model;
+﻿using System.Collections.ObjectModel;
+using System.Windows.Media;
+using SciChart.Charting.Model.ChartSeries;
+using SciChart.Charting.Model.DataSeries;
+using SciChart.Data.Model;
+
+
 namespace SciChart.CTCTutorial
 {
     // Create the MainViewModel. BindableObject is our own viewmodel base class in scichart. You can use your own!
     public class MainViewModel : BindableObject
     {
-        private string _test;
-        public MainViewModel()
+        private string _chartTitle = "Hello SciChart World!";
+        private string _xAxisTitle = "XAxis";
+        private string _yAxisTitle = "YAxis";
+
+        public string ChartTitle
         {
-            TestString = "Hello MVVM World!";
-        }
-        public string TestString
-        {
-            get { return _test; }
+            get { return _chartTitle; }
             set
             {
-                _test = value;
-                OnPropertyChanged("TestString");
+                _chartTitle = value;
+                OnPropertyChanged("ChartTitle");
+            }
+        }
+        public string XAxisTitle
+        {
+            get { return _xAxisTitle; }
+            set
+            {
+                _xAxisTitle = value;
+                OnPropertyChanged("XAxisTitle");
+            }
+        }
+        public string YAxisTitle
+        {
+            get { return _yAxisTitle; }
+            set
+            {
+                _yAxisTitle = value;
+                OnPropertyChanged("YAxisTitle");
             }
         }
     }
