68b7da4328bfb315960821bd1d252e5cc3256c0e
surbachCTC <83680338+surbachCTC@users.noreply.github.com>
2021-05-07 09:39:09 -0700

    Tutorial 09b - Linking Multiple Charts with MVVM

--- SciChart.CTCTutorial/MainViewModel.cs
+++ SciChart.CTCTutorial/MainViewModel.cs
@@ -12,137 +12,16 @@ namespace SciChart.CTCTutorial
     // Create the MainViewModel. BindableObject is our own viewmodel base class in scichart. You can use your own!
     public class MainViewModel : BindableObject
     {
-        private string _chartTitle = "Hello SciChart World!";
-        private string _xAxisTitle = "XAxis";
-        private string _yAxisTitle = "YAxis";
-        private ObservableCollection<IRenderableSeriesViewModel> _renderableSeries = new ObservableCollection<IRenderableSeriesViewModel>();
         private bool _enablePan;
         private bool _enableZoom = true;
-        private XyDataSeries<double, double> _lineData;
-        private DummyDataProvider _dummyDataProvider = new DummyDataProvider();
-        private ObservableCollection<IAnnotationViewModel> _annotations = new ObservableCollection<IAnnotationViewModel>();
-
-        private ObservableCollection<IAxisViewModel> _yAxes = new ObservableCollection<IAxisViewModel>();
-        private ObservableCollection<IAxisViewModel> _xAxes = new ObservableCollection<IAxisViewModel>();
+        private readonly ObservableCollection<ChartViewModel> _chartViewModels = new ObservableCollection<ChartViewModel>();
         public MainViewModel()
         {
-            CreateChartData();
-            CreateChartSeries();
-            CreateChartAxis();
-
-            // Subscribe to future updates
-            int i = 0;
-            _dummyDataProvider.SubscribeUpdates((newValues) =>
-            {
-                // Append when new values arrive
-                _lineData.Append(newValues.XValues, newValues.YValues);
-                // Zoom the chart to fit
-                _lineData.InvalidateParentSurface(RangeMode.ZoomToFit);
-                // Every 100th datapoint, add an annotation
-                if (i % 100 == 0)
-                {
-                    Annotations.Add(new InfoAnnotationViewModel() { X1 = _lineData.XValues.Last(), Y1 = 0.0 });
-                }
-                i++;
-            });
-        }
-        private void CreateChartAxis()
-        {
-            YAxes.Add(new NumericAxisViewModel()
-            {
-                AutoRange = AutoRange.Always,
-                AxisTitle = "Left YAxis",
-                Id = "LeftYAxis",
-                AxisAlignment = AxisAlignment.Left,
-            });
-            YAxes.Add(new NumericAxisViewModel()
-            {
-                AutoRange = AutoRange.Always,
-                AxisTitle = "Right YAxis",
-                AxisAlignment = AxisAlignment.Right,
-            });
-            XAxes.Add(new NumericAxisViewModel()
-            {
-                AutoRange = AutoRange.Always,
-                AxisTitle = "XAxis",
-                AxisAlignment = AxisAlignment.Bottom,
-            });
+            ChartPanes.Add(new ChartViewModel(new DummyDataProvider(), "Primary Chart", "MouseGroup1"));
+            ChartPanes.Add(new ChartViewModel(new DummyDataProvider(), "Secondary Chart", "MouseGroup1"));
         }
 
-        public ObservableCollection<IAxisViewModel> YAxes { get { return _yAxes; } }
-
-        public ObservableCollection<IAxisViewModel> XAxes { get { return _xAxes; } }
-
-
-        private void CreateChartData()
-        {
-            var initialDataValues = _dummyDataProvider.GetHistoricalData();
-            // Create a DataSeries. We later apply this to a RenderableSeries
-            _lineData = new XyDataSeries<double, double>() { SeriesName = "TestingSeries" };
-            // Append some data to the chart                                
-            _lineData.Append(initialDataValues.XValues, initialDataValues.YValues);
-
-        }
-        private void CreateChartSeries()
-        {
-            // Create a RenderableSeries. Apply the DataSeries created before
-            _renderableSeries = new ObservableCollection<IRenderableSeriesViewModel>();
-            RenderableSeries.Add(new LineRenderableSeriesViewModel()
-            {
-                StrokeThickness = 2,
-                Stroke = Colors.SteelBlue,
-                DataSeries = _lineData,
-                StyleKey = "LineSeriesStyle"
-            });
-        }
-        public ObservableCollection<IAnnotationViewModel> Annotations
-        {
-            get { return _annotations; }
-            set
-            {
-                _annotations = value;
-                OnPropertyChanged("Annotations");
-            }
-        }
-        public ObservableCollection<IRenderableSeriesViewModel> RenderableSeries
-        {
-            get { return _renderableSeries; }
-            set
-            {
-                _renderableSeries = value;
-                OnPropertyChanged("RenderableSeries");
-            }
-        }
-
-
-
-        public string ChartTitle
-        {
-            get { return _chartTitle; }
-            set
-            {
-                _chartTitle = value;
-                OnPropertyChanged("ChartTitle");
-            }
-        }
-        public string XAxisTitle
-        {
-            get { return _xAxisTitle; }
-            set
-            {
-                _xAxisTitle = value;
-                OnPropertyChanged("XAxisTitle");
-            }
-        }
-        public string YAxisTitle
-        {
-            get { return _yAxisTitle; }
-            set
-            {
-                _yAxisTitle = value;
-                OnPropertyChanged("YAxisTitle");
-            }
-        }
+        public ObservableCollection<ChartViewModel> ChartPanes { get { return _chartViewModels; } }
 
         public bool EnableZoom
         {
@@ -171,10 +50,6 @@ namespace SciChart.CTCTutorial
                 }
             }
         }
-
-
     }
-
 }
-
 
